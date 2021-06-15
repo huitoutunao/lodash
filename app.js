678a8a0d980725bc861a433ccd86cbd693c89460
huitoutunao <lh_huitoutunao@163.com>
2021-06-15 23:56:57 +0800

    feat: 完成node调试环境配置

---
+++ app.js
@@ -0,0 +1,11 @@
+import add from './add'
+// import debounce from './debounce'
+
+console.log(add(1, 2))
+
+// function test() {
+//   console.log('debounce')
+// }
+
+// const fn = debounce(test, 1000)
+// fn()
